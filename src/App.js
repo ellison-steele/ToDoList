d5fb81166ca971cb1485b19ac5d92056c99177cc
Ellison Steele <ellison.steele@gmail.com>
2021-09-27 00:34:09 -0400

    adding main application files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import './components/TodoForm'
+import TodoList from './components/TodoList';
+
+
+
+
+
+function App() {
+  return (
+    <><div className='todo-app'>
+      <TodoList />
+    </div></>
+
+  );
+}
+
+export default App;
